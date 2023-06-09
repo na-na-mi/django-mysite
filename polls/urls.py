d805b37e8a7b85fd5478a9c0a66f77c8afec9f91
帅气的机智君 <ljz400216@gmail.com>
2023-06-09 20:23:53 +0800

    实现了多个页面的显示

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import visualize_rules
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("index/", views.index),
+    path("index/", views.index, name="index"),
+
+    path("test/", views.test, name="test"),
+    path('visualize/', visualize_rules, name='visualize_rules'),
 ]
